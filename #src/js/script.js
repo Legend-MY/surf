813e8b93702b2ad8e7a6084e0b64b603eb8e9ec5
Legend-MY <markov2008@bigmir.net>
2020-10-02 00:26:48 +0300

    Начал делать header. Подключение второго слайдера

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,1 +1,16 @@
+@@include('slick.min.js');
 
+$(document).ready(function () {
+  $('.header__slider').slick({
+    infinite: true,
+    fade: true,
+    asNavFor: '.header__slider-dots',
+  });
+
+  $('.header__slider-dots').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: '.header__slider',
+  });
+
+});
