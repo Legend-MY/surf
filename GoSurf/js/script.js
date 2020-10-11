29828132404230233a6aab9b0986ff0ef4568a40
Legend-MY <markov2008@bigmir.net>
2020-10-11 22:10:27 +0300

    Сделал блок Travel и сделал адаптив

--- GoSurf/js/script.js
+++ GoSurf/js/script.js
@@ -103,4 +103,48 @@ $(document).ready(function () {
     infinite: true,
     fade: true,
   });
+
+
+
+  $('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="img/sleep/plus.png" alt="add"></div><div class="quantity-button quantity-down"><img src="img/sleep/minus.png" alt="remove"></div></div>').insertAfter('.quantity input');
+  $('.quantity').each(function () {
+    var spinner = $(this),
+      input = spinner.find('input[type="number"]'),
+      btnUp = spinner.find('.quantity-up'),
+      btnDown = spinner.find('.quantity-down'),
+      min = input.attr('min'),
+      max = input.attr('max');
+
+    btnUp.click(function () {
+      var oldValue = parseFloat(input.val());
+      if (oldValue >= max) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue + 1;
+      }
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
+    });
+
+    btnDown.click(function () {
+      var oldValue = parseFloat(input.val());
+      if (oldValue <= min) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue - 1;
+      }
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
+    });
+
+  });
+
+
+
+  $('.quantity-button').on('click', function () {
+    let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests');
+    $('.summ').html('$' + summ);
+  });
+  let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests');
+  $('.summ').html('$' + summ);
 });
